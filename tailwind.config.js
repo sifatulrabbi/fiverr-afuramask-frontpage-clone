b4db44610319c19e2b552e9fbe152e82910d788d
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-03-31 13:18:40 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        "./pages/**/*.{tsx,ts,js,jsx}",
+        "./components/**/*.{tsx,ts,jsx,js}",
+    ],
+    theme: {
+        fontFamily: {
+            sans: ["Noto Sans Japanese", "sans-serif"],
+            serif: ["Noto Serif Japanese", "serif"],
+        },
+        colors: {
+            primary: "#ff413a",
+        },
+    },
+    plugins: [],
+};
