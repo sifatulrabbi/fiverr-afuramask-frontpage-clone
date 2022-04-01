9288d325abfc27769e0525b3357f6a1df2c987c2
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-04-01 15:20:24 +0600

    add images, restructure assets folder

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+export { default as logo } from "./Logo.png";
+export { default as heroImg } from "./hero-img.png";
+export { default as casinoImg1 } from "./casino-img-1.png";
+export { default as girlHappy } from "./girl-happy.png";
+export { default as girlSmile } from "./girl-smile.png";
+export { default as girlTalk } from "./girl-talks.png";
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,2 @@
-export { default as logo } from "./Logo.png";
-export { default as heroImg } from "./hero-img.png";
-export { default as casinoImg1 } from "./casino-img-1.png";
+export * from "./images";
 export * from "./data";
