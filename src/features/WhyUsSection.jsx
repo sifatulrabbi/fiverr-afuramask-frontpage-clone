d05912560a8399f4a3af21305d11c09496d4bc06
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-04-01 17:04:22 +0600

    refactor: why us section

--- src/features/WhyUsSection.jsx
+++ src/features/WhyUsSection.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { girlHappy } from "../assets";
 
+const ListItem = ({ text }) => {
+    return (
+        <p className="pl-3 gap-4 my-4">
+            <span className="h-2 w-2 rounded-full bg-primary inline-block mr-4" />
+            {text}
+        </p>
+    );
+};
+
 export const WhyUsSection = () => {
     return (
         <section className="flex flex-col justify-start items-start overflow-hidden bg-gradient p-3 mt-10 rounded-2xl pb-[250px]">
@@ -16,22 +25,29 @@ export const WhyUsSection = () => {
                     このページでは、プロセスの各ステップについて説明します。これにより、大吉カジノのチームが探しているものと、各オンラインカジノ
                     日本をどのようにテストするかを確認できます。
                 </p>
-                <p>評判</p>
+                <ListItem text={"評判"} />
                 <p>
                     ✅
                     そこから、すべてのサイトが迅速な引き出しと公正なRTPで高いペイアウト率を提供することが不可欠です。
                 </p>
-                <p>
-                    バンキングと入<a href="/">出金方法</a>
-                    すべての日本オンラインギャンブルサイトに必要なのは、安全な銀行のオプションの範囲です。
-                </p>
+                <ListItem
+                    text={
+                        <>
+                            バンキングと入
+                            <a href="/" className="text-primary">
+                                出金方法
+                            </a>
+                            すべての日本オンラインギャンブルサイトに必要なのは、安全な銀行のオプションの範囲です
+                        </>
+                    }
+                />
                 <p>
                     ✅
                     そこから、すべてのサイトが迅速な引き出しと公正なRTPで高いペイアウト率を提供することが不可欠です。
                 </p>
                 <img
                     src={girlHappy}
-                    alt=""
+                    alt="オンラインカジノをどのように評価しますか？"
                     className="object-cover absolute bottom-0 translate-y-full left-1/2 -translate-x-1/2"
                 />
             </article>
