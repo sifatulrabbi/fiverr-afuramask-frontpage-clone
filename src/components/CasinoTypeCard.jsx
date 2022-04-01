d32e32079efddb3a1d334f7e91a22f616e5fc755
sifatulrabbi <mdsifatulislam.rabbi@gmail.com>
2022-04-01 23:17:16 +0600

    feat: add Casino types section

---
+++ src/components/CasinoTypeCard.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CasinoTypeCard = () => {
+    return <div>CasinoTypeCard</div>;
+};
---
+++ src/features/CasinoTypeSection.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CasinoTypeSection = () => {
+    return <div>CasinoTypeSection</div>;
+};
